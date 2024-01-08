0603a5e88316f4e69bcca84b0fc3547747e885da
GandaMitsuo <panpeikun@web.de>
2024-01-08 20:19:00 +0100

    add user btn

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule, MatButtonModule, MatTooltipModule, FormsModule, ReactiveFormsModule],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
 })
 export class UserComponent {
+  position: any;
 
 }
