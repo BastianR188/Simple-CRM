5da1c09ed8d23347ed039f9ec0d821f734786303
Riedmann Bastian <Riedmann.Basti@gmail.com>
2024-01-22 18:09:01 +0100

    add imprint

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MyServiceService } from '../firestore.service';
 import { ActivatedRoute, Router } from '@angular/router';
@@ -9,30 +9,55 @@ import { MatMenuModule } from '@angular/material/menu';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogEditUserComponent } from '../dialog-edit-user/dialog-edit-user.component';
 import { DialogEditAddressComponent } from '../dialog-edit-address/dialog-edit-address.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-user-detail',
   standalone: true,
-  imports: [MatCardModule, MatIconModule, MatButtonModule, MatMenuModule],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule,
+    MatMenuModule,
+  ],
   providers: [DialogEditAddressComponent, DialogEditUserComponent],
   templateUrl: './user-detail.component.html',
-  styleUrl: './user-detail.component.scss'
+  styleUrl: './user-detail.component.scss',
 })
 export class UserDetailComponent {
-
   user: User = new User();
   id: any;
-
-
-  constructor(private service: MyServiceService, private route: ActivatedRoute, private router: Router, public dialog: MatDialog) {
-
-  }
+  neueBildquelle: string = '';
+  constructor(
+    private service: MyServiceService,
+    private route: ActivatedRoute,
+    private router: Router,
+    public dialog: MatDialog
+  ) {}
+  @ViewChild('imagePic') imagePic!: ElementRef;
 
   ngOnInit() {
-    this.route.paramMap.subscribe(paramMap => {
+    this.route.paramMap.subscribe((paramMap) => {
       this.id = paramMap.get('id');
       this.getUserData(this.id);
-    })
+      this.loadimg(this.id);
+    });
+  }
+
+  changeImage() {
+    this.imagePic.nativeElement.src = this.neueBildquelle;
+  }
+
+  loadimg(id: string) {
+    let img = new Image();
+    img.src = './assets/img/' + id + '.jpg';
+    img.onerror = () => {
+      return console.log('Image does not exist');
+    };
+    img.onload = () => {
+      this.imagePic.nativeElement.src = img.src;
+    };
   }
 
   deleteUser(userId: string) {
@@ -49,16 +74,16 @@ export class UserDetailComponent {
     let dialog = this.edit(edit);
     if (dialog) {
       dialog.componentInstance.user = new User(this.user);
-      dialog.componentInstance.userId = this.id
-      dialog.afterClosed().subscribe(result => {
+      dialog.componentInstance.userId = this.id;
+      dialog.afterClosed().subscribe((result) => {
         if (result) this.user = result;
       });
     }
-
   }
 
   edit(edit: string) {
-    if (edit == 'user') { return this.dialog.open(DialogEditUserComponent); }
-    else return this.dialog.open(DialogEditAddressComponent);
+    if (edit == 'user') {
+      return this.dialog.open(DialogEditUserComponent);
+    } else return this.dialog.open(DialogEditAddressComponent);
   }
 }
